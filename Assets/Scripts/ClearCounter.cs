6dfaaa7a024254337107457ef3449a0a827bd3af
Raahim Fareed <raahimfareed@proton.me>
2023-12-11 23:46:08 +0500

    Refactored

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class ClearCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
-
     public override void Interact(Player player)
     {
         if (!HasKitchenObject())
